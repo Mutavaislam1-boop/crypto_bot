8ff6968902c0722898b0e97edddeb240ba70d1f9
Mutavaislam1-boop <mutavaislam1@gmail.com>
2026-06-10 17:19:45 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -233,28 +233,6 @@ def check_signals():
             current_price
         )
 
-    if "NO TRADE" in signal[5]:
-
-        if result_percent > 3:
-            verdict = "❌ BOT MISSED PROFIT"
-
-        elif result_percent < -3:
-            verdict = "✅ NO TRADE CORRECT"
-
-        else:
-            verdict = "⚪ NEUTRAL"
-
-    else:
-
-        if result_percent > 3:
-            verdict = "✅ BUY CORRECT"
-
-        elif result_percent < -3:
-            verdict = "❌ BUY FAILED"
-
-        else:
-            verdict = "⚪ NEUTRAL"  
-
     print(
         f"Signal #{signal_id} | "
         f"{symbol} | "
